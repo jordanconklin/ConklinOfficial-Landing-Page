3f10bcd13c318dad1d95b5516550187881da5ef9
jordanconklin <jordinho@ucla.edu>
2024-09-17 16:43:32 -0700

    separating backend and frontend

---
+++ spring-backend/src/main/java/Tekk/SpringbootStripe/SpringbootStripeApplication.java
@@ -0,0 +1,13 @@
+package Tekk.SpringbootStripe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStripeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStripeApplication.class, args);
+	}
+
+}
