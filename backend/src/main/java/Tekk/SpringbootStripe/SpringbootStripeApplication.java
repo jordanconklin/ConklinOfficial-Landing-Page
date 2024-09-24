b31135c0abce9f7694f2db459a5092ad6c7d6fc7
jordanconklin <jordinho@ucla.edu>
2024-09-24 15:58:37 -0700

    update commit, security check

--- backend/src/main/java/Tekk/SpringbootStripe/SpringbootStripeApplication.java
+++ backend/src/main/java/Tekk/SpringbootStripe/SpringbootStripeApplication.java
@@ -3,6 +3,7 @@ package Tekk.SpringbootStripe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// This is the main class that runs the Spring Boot application
 @SpringBootApplication
 public class SpringbootStripeApplication {
 
