0c3f73fbe2107fa2950b48886e1310b160757c9a
jordanconklin <jordinho@ucla.edu>
2024-10-19 14:05:18 -0700

    commiting to test email sending in production

--- backend/src/main/java/Tekk/SpringbootStripe/controller/EmailController.java
+++ backend/src/main/java/Tekk/SpringbootStripe/controller/EmailController.java
@@ -1,17 +1,28 @@
 package Tekk.SpringbootStripe.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import Tekk.SpringbootStripe.model.EmailRequest;
+import Tekk.SpringbootStripe.service.EmailService;
 
 @RestController
 public class EmailController {
 
+    @Autowired
+    private EmailService emailService;
+
     @PostMapping("/api/send-email")
     public String sendEmail(@RequestBody EmailRequest request) {
-        // Mock response for now
-        System.out.println("Received email request: " + request.getName() + ", " + request.getEmail());
-        return "Email request received successfully (Note: Emails are not actually sent in production)";
+        String to = "conklinofficialsoccer@gmail.com";
+        String subject = "New message from " + request.getName();
+        String body = String.format("Name: %s\nEmail: %s\nMessage: %s", 
+                                    request.getName(), 
+                                    request.getEmail(), 
+                                    request.getMessage());
+            
+        emailService.sendEmail(to, subject, body);
+        return "Email sent successfully";
     }
 }
